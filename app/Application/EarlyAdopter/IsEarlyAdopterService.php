497e4dd0a89edac950190fdbcec61f7c09ac2ea5
crackergirl <oihanaerroalzueta@gmail.com>
2022-04-07 11:07:57 +0000

    [1] - GET /api/{anything} Generic Error

--- app/Application/EarlyAdopter/IsEarlyAdopterService.php
+++ app/Application/EarlyAdopter/IsEarlyAdopterService.php
@@ -37,4 +37,5 @@ class IsEarlyAdopterService
 
         return $isEarlyAdopter;
     }
+
 }
--- app/Infrastructure/Controllers/GetUserController.php
+++ app/Infrastructure/Controllers/GetUserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Infrastructure\Controllers;
 
+use App\Application\UserDataSource\UserDataSource;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
+use function PHPUnit\Framework\isEmpty;
 
 class GetUserController extends BaseController
 {
+
     public function __invoke(string $userId): JsonResponse
     {
+
         return response()->json([
             'error' => "user does not exist"
         ], Response::HTTP_BAD_REQUEST);
